aa4fbc28593b3e9548bf2f69f3cf28fa916dff92
David Cantoran <davidcantoran36@gmail.com>
2019-12-28 00:45:34 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,6 +1,17 @@
 class Course 
   attr_accessor :title, :schedule, :description
   @@all = []
+  def initialize(title, schedule, description)
+    @title = title
+    @schedule = schedule 
+    @description = description
+    save
+  end
+  
+  def save
+    @@all << self
+  end 
+  
   def self.reset_all
     @@all.clear
   end 
